b38cd592f235b69eb8e5003b2aab75b42a420e36
Arĥimedeς ℳontegasppα ℭacilhας <batalema@cacilhas.info>
2018-08-28 22:51:44 -0300

    Use user widget

--- id_profile/views.py
+++ id_profile/views.py
@@ -6,4 +6,5 @@ from nexaas_id_client.support.django.decorators import authorization_required
 def index(request, api_client: NexaasIDClient):
     return render(request, 'id_profile/index.html', {
         'user': api_client.personal_info,
+        'widget': api_client.user_widget_url,
     })
