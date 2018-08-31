5d35aead61aa9d2f24de2c8ef841d67aa551f9f4
Rodrigo Cacilhas <rodrigo.cacilhas@nexaas.com>
2018-08-31 17:54:19 -0300

    Match client update

--- id_profile/views.py
+++ id_profile/views.py
@@ -1,13 +1,9 @@
-from django.shortcuts import redirect, render, reverse
+from django.shortcuts import render
 from nexaas_id_client.support.django.decorators import authorization_required
-from nexaas_id_client.support.django.helpers import signed_in
 
 @authorization_required
 def index(request, api_client):
-    res = None
-    with signed_in() as unsigned:
-        res = render(request, 'id_profile/index.html', {
-            'user': api_client.personal_info,
-            'widget': api_client.user_widget_url,
-        })
-    return res or unsigned.redirect
+    return render(request, 'id_profile/index.html', {
+        'user': api_client.personal_info,
+        'widget': api_client.user_widget_url,
+    })
